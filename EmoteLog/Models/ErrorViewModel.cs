8c27c1737c8f8644823fcd280c8865d5829fcd80
Katie Strauss <straussk@my.lanecc.edu>
2019-12-08 12:16:57 -0800

    Add project files.

---
+++ EmoteLog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmoteLog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
